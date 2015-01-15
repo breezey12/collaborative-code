a1aef5e473bae521915abbbbb21568f401b44807
Zev Averbach <zev@avtranscription.com>
2015-01-15 11:18:22 -0500

    cleaned up tryingArgparse.py, putting all non-functions in 'if __name__ == "main"' function

--- tryingArgparse.py
+++ tryingArgparse.py
@@ -1,19 +1,3 @@
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('startingNumber', type=int)
-parser.add_argument('endingNumber', type=int, help="this is the last number \
-that'll be counted")
-parser.add_argument('-even', action="store_true", help="display only even \
-numbers")
-parser.add_argument('-countby', type=int)
-args = parser.parse_args()
-
-end = args.endingNumber
-start = args.startingNumber
-even = args.even
-countBy = args.countby
-
-
 def evenF(number):
     if number % 2 == 0:
         return number
@@ -43,4 +27,17 @@ def counting(a, z):
             print result
 
 if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('startingNumber', type=int)
+    parser.add_argument('endingNumber', type=int, help="this is the last number \
+    that'll be counted")
+    parser.add_argument('-even', action="store_true", help="display only even \
+    numbers")
+    parser.add_argument('-countby', type=int)
+    args = parser.parse_args()
+    end = args.endingNumber
+    start = args.startingNumber
+    even = args.even
+    countBy = args.countby
     counting(start, end)
